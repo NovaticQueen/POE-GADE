7bc1b07714913999b0ff21924441a1c980cefb54
NovaticQueen <55442720+NovaticQueen@users.noreply.github.com>
2019-10-22 23:13:25 +0200

    Initial commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas MainCanvas;
+    public Canvas LoadCanvas;
+    public Canvas MapCanvas;
+
+    private void Awake() //This method starts before the start method
+    {
+        LoadCanvas.enabled = false;
+        MapCanvas.enabled = false;
+    }
+
+    public void LoadMenuOn() //Loading menu button
+    {
+        LoadCanvas.enabled = true;
+        MainCanvas.enabled = false;
+        MapCanvas.enabled = false;
+    }
+
+    public void ReturnOn() //Return to main menu button 
+    {
+        LoadCanvas.enabled = false;
+        MainCanvas.enabled = true;
+        MapCanvas.enabled = false;
+    }
+
+    public void LoadOn() //Load game button
+    {
+        MapCanvas.enabled = true;
+        MainCanvas.enabled = false;
+    }
+}
