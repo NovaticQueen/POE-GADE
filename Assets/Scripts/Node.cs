e9ac2310ed0c23ba4001ea3a90585511089de625
NovaticQueen <55442720+NovaticQueen@users.noreply.github.com>
2019-10-28 11:32:30 +0200

    Overlap
    Fixed overlap on buildings.

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    int x, z;
+    bool isPlaceable = true;
+    Vector3 pos;
+    public Node(int x, int z)
+    {
+        this.x = x;
+        this.z = z;
+        pos = new Vector3(x, 1, z);
+    }
+
+    public bool IsPlaceable
+    {
+        get { return isPlaceable; }
+        set { isPlaceable = value; }
+    }
+
+    public Vector3 Pos
+    {
+        get { return pos; }
+    }
+}
--- Assets/Scripts/PrefabSpawnerBuildings.cs
+++ Assets/Scripts/PrefabSpawnerBuildings.cs
@@ -8,16 +8,18 @@ public class PrefabSpawnerBuildings : MonoBehaviour
     public GameObject resourceB;
 
     MapManager mapManager;
-    List<GameObject> unitList;
     List<GameObject> buildingList;
 
     public int buildingAmount;
+
+    private Node[,] node;
 
     // Start is called before the first frame update
     void Start()
     {
         mapManager = GameObject.FindObjectOfType<MapManager>();
-        SpawnBuildings();
+        //SpawnBuildings();
+        SpawnTest();
     }
     
     void SpawnBuildings()
@@ -45,4 +47,50 @@ public class PrefabSpawnerBuildings : MonoBehaviour
         }
 
     }
+
+    void SpawnTest()
+    {
+        buildingList = new List<GameObject>();
+        node = new Node[mapManager.mapWidth, mapManager.mapLength];
+
+        for (int x = 0; x < mapManager.mapWidth; x++)
+        {
+            for (int z = 0; z < mapManager.mapLength; z++)
+            {
+                node[x, z] = new Node(x, z);
+                node[x, z].IsPlaceable = true;
+            }
+        }
+
+        for (int i = 0; i < buildingAmount; i++)
+        {
+            int buidingType = Random.Range(0, 2);
+            int x = Random.Range(2, mapManager.mapWidth - 1);
+            int z = Random.Range(2, mapManager.mapLength - 1);
+
+            while (!node[x, z].IsPlaceable)
+            {
+                x = Random.Range(2, mapManager.mapWidth - 1);
+                z = Random.Range(2, mapManager.mapLength - 1);
+            }
+
+            node[x, z].IsPlaceable = true;
+            GameObject b;
+
+            if (buidingType == 0) //FactoryBuilding
+            {
+                b = Instantiate(factoryB);
+                b.transform.position = new Vector3(x, 1, z);
+            }
+            else
+            {
+                b = Instantiate(resourceB);
+                b.transform.position = new Vector3(x, 1, z);
+            }
+
+            buildingList.Add(b);
+        }
+    }
+
+    
 }
